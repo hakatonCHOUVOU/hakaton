11e5886af44bc9f180e646714eb9619371d18d33
Cash3mod3l <110199412+Cash3mod3l@users.noreply.github.com>
2022-08-21 18:03:09 +0300

    Добавлена система нодификации

--- electron/test.js
+++ electron/test.js
@@ -2,6 +2,15 @@ const copyButton = document.querySelector("#copyButton");
 const content = document.querySelector("#block");
 const deleteButton = document.querySelector("#deleteButton");
 const sendingServer = document.querySelector("#ToRunServer");
+document.querySelector('body').style.backgroundColor = "white";
+document.querySelector('body').style.color = "black";
+const notifElem = document.querySelector('.nothification');
+const textNorif = document.querySelector("#textNotif");
+
+const notif = {
+    open: (color = 'pink') => notifElem.classList.add('go', color),
+    close: () => notifElem.classList.remove('go')
+}
 
 
 /**
@@ -9,9 +18,17 @@ const sendingServer = document.querySelector("#ToRunServer");
  */
 deleteButton.addEventListener("click", function () {
 
-    content.innerHTML = "";
-    console.log("Текст удалён");
-
+    if (content.innerHTML !== "") {
+        content.innerHTML = "";
+        console.log("Текст удалён");
+        setTimeout(() => {
+            notif.open('green')
+            textNorif.innerHTML = "Текст удалён"
+            setTimeout(() => {
+                notif.close()
+            }, 1700)
+        }, 10)
+    }
 })
 
 /**
@@ -19,18 +36,25 @@ deleteButton.addEventListener("click", function () {
  */
 copyButton.addEventListener("click", function () {
 
-    if (content.innerHTML === "") {
-        console.log("Пустота!!");
-    } else {
-        navigator.clipboard.writeText(content.innerHTML)
-            .then(function () {
-                console.log("Скопировалось");
-            })
-            .catch(err => {
-                console.log("Не скопировалось");
-            })
-    }
-
+    navigator.clipboard.writeText(content.innerHTML)
+        .then(function () {
+            setTimeout(() => {
+                notif.open('green')
+                textNorif.innerHTML = "Текст скопирован"
+                setTimeout(() => {
+                    notif.close()
+                }, 1700)
+            }, 10)
+        })
+        .catch(function () {
+            setTimeout(() => {
+                notif.open('red')
+                textNorif.innerHTML = "Не удалось скопировать текст"
+                setTimeout(() => {
+                    notif.close()
+                }, 1700)
+            }, 10)
+        })
 })
 
 /**
@@ -38,15 +62,28 @@ copyButton.addEventListener("click", function () {
  */
 sendingServer.addEventListener("click", async function () {
 
-    if (content.innerHTML === "") {
-        console.log("Пусто! Отправка не пройдёт");
-    } else {
-        await fetch('https://server-cahef34500.pagekite.me/', {method:'POST', body:JSON.stringify({name: content.innerHTML}), headers:{"Content-type":"application/json"}})
-            .then(async data => {
-                let text = await data.text();
-                console.log("Отправка прошла");
-                console.log(text);
-            })
-    }
-
+    await fetch('https://server-cahef34500.pagekite.me/', {method:'POST', body:JSON.stringify({name: content.innerHTML}), headers:{"Content-type":"application/json"}})
+        .then(async data => {
+            let text = await data.text();
+            setTimeout(() => {
+                notif.open('green')
+                textNorif.innerHTML = "Отправка прошла успешно"
+                setTimeout(() => {
+                    notif.close()
+                }, 1700)
+            }, 10)
+            console.log(text);
+        })
+        .catch(async data => {
+            setTimeout(() => {
+                notif.open('red')
+                textNorif.innerHTML = "Отправка не прошла"
+                setTimeout(() => {
+                    notif.close()
+                }, 1700)
+            }, 10)
+        })
 })
+
+
+window.notif = notif;

f5c69a0de48cb8aac942fc3cfaa4a90c9d3cc6f9
Cash3mod3l <110199412+Cash3mod3l@users.noreply.github.com>
2022-08-21 21:04:03 +0300

    Поправка кода

--- electron/test.js
+++ electron/test.js
@@ -2,88 +2,101 @@ const copyButton = document.querySelector("#copyButton");
 const content = document.querySelector("#block");
 const deleteButton = document.querySelector("#deleteButton");
 const sendingServer = document.querySelector("#ToRunServer");
-document.querySelector('body').style.backgroundColor = "white";
-document.querySelector('body').style.color = "black";
 const notifElem = document.querySelector('.nothification');
 const textNorif = document.querySelector("#textNotif");
+const dowlandDocx = document.querySelector("#dowlanddoc");
 
 const notif = {
     open: (color = 'pink') => notifElem.classList.add('go', color),
     close: () => notifElem.classList.remove('go')
 }
 
+function ShowNoficationDeleteText() {
+    setTimeout(() => {
+        notif.open('green')
+        textNorif.innerHTML = "Текст удалён"
+
+        setTimeout(() => {
+            notif.close()
+        }, 1700)
+    }, 10)
+}
+
+function CopyText(){
+    setTimeout(() => {
+        notif.open('green')
+        textNorif.innerHTML = "Текст скопирован"
+        
+        setTimeout(() => {
+            notif.close()
+        }, 1700)
+    }, 10)
+}
+
+function ShowNotificationSending() {
+    setTimeout(() => {
+        notif.open('green')
+        textNorif.innerHTML = "Отправка прошла успешно"
+
+        setTimeout(() => {
+            notif.close()
+        }, 1700)
+    }, 10)
+}
+
+function ShowNotNotificationSending() {
+    setTimeout(() => {
+        notif.open('red')
+        textNorif.innerHTML = "Отправка не прошла"
+
+        setTimeout(() => {
+            notif.close()
+        }, 1700)
+    }, 10)
+}
+
 
 /**
  * Метод для удаление текста
  */
-deleteButton.addEventListener("click", function () {
+deleteButton.onclick = function () {
 
-    if (content.innerHTML !== "") {
-        content.innerHTML = "";
-        console.log("Текст удалён");
-        setTimeout(() => {
-            notif.open('green')
-            textNorif.innerHTML = "Текст удалён"
-            setTimeout(() => {
-                notif.close()
-            }, 1700)
-        }, 10)
+    if (content.innerHTML === "") {
+        return;
     }
-})
+    content.innerHTML = "";
+    console.log("Текст удалён");
+    ShowNoficationDeleteText();
+}
 
 /**
  * Метод для копирования текста
  */
-copyButton.addEventListener("click", function () {
-
+copyButton.onclick = function () {
     navigator.clipboard.writeText(content.innerHTML)
         .then(function () {
-            setTimeout(() => {
-                notif.open('green')
-                textNorif.innerHTML = "Текст скопирован"
-                setTimeout(() => {
-                    notif.close()
-                }, 1700)
-            }, 10)
+            CopyText();
         })
-        .catch(function () {
-            setTimeout(() => {
-                notif.open('red')
-                textNorif.innerHTML = "Не удалось скопировать текст"
-                setTimeout(() => {
-                    notif.close()
-                }, 1700)
-            }, 10)
-        })
-})
+}
 
 /**
  * Метод для отправки данных на серверя
  */
-sendingServer.addEventListener("click", async function () {
+sendingServer.onclick = async function() {
 
-    await fetch('https://server-cahef34500.pagekite.me/', {method:'POST', body:JSON.stringify({name: content.innerHTML}), headers:{"Content-type":"application/json"}})
+    await fetch('https://server-cahef34500.pagekite.me/', {
+        method: 'POST',
+        body: JSON.stringify({name: content.innerHTML}),
+        headers: {"Content-type": "application/json"}
+    })
         .then(async data => {
             let text = await data.text();
-            setTimeout(() => {
-                notif.open('green')
-                textNorif.innerHTML = "Отправка прошла успешно"
-                setTimeout(() => {
-                    notif.close()
-                }, 1700)
-            }, 10)
+            ShowNotificationSending();
             console.log(text);
         })
         .catch(async data => {
-            setTimeout(() => {
-                notif.open('red')
-                textNorif.innerHTML = "Отправка не прошла"
-                setTimeout(() => {
-                    notif.close()
-                }, 1700)
-            }, 10)
+            ShowNotNotificationSending()
         })
-})
-
+}
 
 window.notif = notif;

97d65fce58eaf13a4877a0395f59677793dab74c
Cash3mod3l <110199412+Cash3mod3l@users.noreply.github.com>
2022-08-23 19:30:57 +0300

    Добавлена анимация кнопок и наудификация
    я забыл как пишется эта херня

--- electron/test.js
+++ electron/test.js
@@ -1,73 +1,89 @@
-const copyButton = document.querySelector("#copyButton");
-const content = document.querySelector("#block");
-const deleteButton = document.querySelector("#deleteButton");
-const sendingServer = document.querySelector("#ToRunServer");
-const notifElem = document.querySelector('.nothification');
-const textNorif = document.querySelector("#textNotif");
-const dowlandDocx = document.querySelector("#dowlanddoc");
+const copyButton    = document.querySelector('#copyButton');
+const content       = document.querySelector('#block');
+const deleteButton  = document.querySelector('#deleteButton');
+const sendingServer = document.querySelector('#ToRunServer');
+const notifElem     = document.querySelector('.nothification');
+const textNorif     = document.querySelector('#textNotif');
+const buttonText    = document.querySelector('#buttonText');
+const textBut       = document.querySelector('#ShowTextBut');
 
 const notif = {
-    open: (color = 'pink') => notifElem.classList.add('go', color),
-    close: () => notifElem.classList.remove('go')
+    open  : (color = 'pink') => notifElem.classList.add('showNotification', color),
+    close : () => notifElem.classList.remove('showNotification'),
+};
+
+const butText = {
+    open  : (color = 'aqua') => buttonText.classList.add('showText'),
+    close : () => buttonText.classList.remove('showText'),
+};
+
+function GreateShowTextButton() {
+    setTimeout(() => {
+        butText.open('aqua');
+        textBut.innerHTML = 'Игорь пидор';
+
+        setTimeout(function (event) {
+            butText.close();
+        }, 1700);
+    }, 10);
 }
 
 function ShowNoficationDeleteText() {
     setTimeout(() => {
-        notif.open('green')
-        textNorif.innerHTML = "Текст удалён"
+        notif.open('green');
+        textNorif.innerHTML = 'Текст удалён';
 
         setTimeout(() => {
-            notif.close()
-        }, 1700)
-    }, 10)
+            notif.close();
+        }, 1700);
+    }, 10);
 }
 
-function CopyText(){
+function CopyText() {
     setTimeout(() => {
-        notif.open('green')
-        textNorif.innerHTML = "Текст скопирован"
-        
+        notif.open('green');
+        textNorif.innerHTML = 'Текст скопирован';
+
         setTimeout(() => {
-            notif.close()
-        }, 1700)
-    }, 10)
+            notif.close();
+        }, 1700);
+    }, 10);
 }
 
 function ShowNotificationSending() {
     setTimeout(() => {
-        notif.open('green')
-        textNorif.innerHTML = "Отправка прошла успешно"
+        notif.open('green');
+        textNorif.innerHTML = 'Отправка прошла успешно';
 
         setTimeout(() => {
-            notif.close()
-        }, 1700)
-    }, 10)
+            notif.close();
+        }, 1700);
+    }, 10);
 }
 
 function ShowNotNotificationSending() {
     setTimeout(() => {
-        notif.open('red')
-        textNorif.innerHTML = "Отправка не прошла"
+        notif.open('red');
+        textNorif.innerHTML = 'Отправка не прошла';
 
         setTimeout(() => {
-            notif.close()
-        }, 1700)
-    }, 10)
+            notif.close();
+        }, 1700);
+    }, 10);
 }
-
 
 /**
  * Метод для удаление текста
  */
 deleteButton.onclick = function () {
 
-    if (content.innerHTML === "") {
+    if (content.innerHTML === '') {
         return;
     }
-    content.innerHTML = "";
-    console.log("Текст удалён");
+    content.innerHTML = '';
+    console.log('Текст удалён');
     ShowNoficationDeleteText();
-}
+};
 
 /**
  * Метод для копирования текста
@@ -76,18 +92,18 @@ copyButton.onclick = function () {
     navigator.clipboard.writeText(content.innerHTML)
         .then(function () {
             CopyText();
-        })
-}
+        });
+};
 
 /**
  * Метод для отправки данных на серверя
  */
-sendingServer.onclick = async function() {
+sendingServer.onclick = async function () {
 
     await fetch('https://server-cahef34500.pagekite.me/', {
-        method: 'POST',
-        body: JSON.stringify({name: content.innerHTML}),
-        headers: {"Content-type": "application/json"}
+        method  : 'POST',
+        body    : JSON.stringify({name : content.innerHTML}),
+        headers : {'Content-type' : 'application/json'},
     })
         .then(async data => {
             let text = await data.text();
@@ -95,8 +111,34 @@ sendingServer.onclick = async function() {
             console.log(text);
         })
         .catch(async data => {
-            ShowNotNotificationSending()
-        })
-}
+            ShowNotNotificationSending();
+        });
+};
+
+sendingServer.onclick = function (event) {
+    buttonText.onclick = function () {
+        content.innerHTML = textBut.innerHTML;
+    };
+    GreateShowTextButton();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 window.notif = notif;

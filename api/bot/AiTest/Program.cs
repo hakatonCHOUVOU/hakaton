d7fced70dd73390c3d7c1e45b8abc0e3fc0c739a
Cash3mod3l <110199412+Cash3mod3l@users.noreply.github.com>
2022-08-27 19:30:51 +0300

    Добавляет файл C#

---
+++ api/bot/AiTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AiTest
+{
+    public class Program
+    {
+        static string Test()
+        {
+            return "Hello World";
+        }
+
+        public static void Main()
+        {
+            Console.Write(Test());
+        }
+    }
+}
